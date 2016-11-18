d74e6df026836e6dcb2d7fbc5c6027f5ddbd1e3f
mchmielarz <Michal.Chmielarz@4finance.com>
2016-11-18 15:41:46 +0100

    Adjusted version of PageAccessToken object

--- src/main/java/com/jvm_bloggers/facebook/client/fb/PageAccessToken.java
+++ src/main/java/com/jvm_bloggers/facebook/client/fb/PageAccessToken.java
@@ -1,15 +1,19 @@
 package com.jvm_bloggers.facebook.client.fb;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.NoArgsConstructor;
 
 import static lombok.AccessLevel.PRIVATE;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor(access = PRIVATE)
 public class PageAccessToken {
 
-    private String access_token;
+    @JsonProperty("access_token")
+    private String token;
 
     public String getToken() {
-        return access_token;
+        return token;
     }
 }
